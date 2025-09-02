9a7cae6d2c93f48ce0e12ad16a4419bd4c45b3cd
KaitoTLex <renl@kaitotlex.systems>
2025-09-02 15:47:28 -0700

    feat: support for ame

--- flake.nix
+++ flake.nix
@@ -91,7 +91,7 @@
           # settings for stateful data, like file locations and database versions
           # on your system were taken. It‘s perfectly fine and recommended to leave
           # this value at the release version of the first install of home-manager.
-          home.stateVersion = "25.11";
+          home.stateVersion = "25.05";
         };
       };
     in
@@ -185,7 +185,8 @@
               environment.systemPackages = [
                 inputs.nixvim.packages.${pkgs.stdenv.targetPlatform.system}.default
                 pkgs.supergfxctl
-              ] ++ kaitoPkgs;
+              ]
+              ++ kaitoPkgs;
               services.keyd = {
                 enable = true;
                 keyboards.default = {
@@ -233,7 +234,7 @@
               # this value at the release version of the first install of this system.
               # Before changing this value read the documentation for this option
               # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-              system.stateVersion = "25.11"; # Did you read the comment?
+              system.stateVersion = "25.05"; # Did you read the comment?
 
               # Other options such as
               # hardware.graphics.enable = true
@@ -360,7 +361,8 @@
               # Set up a bootloader:
               environment.systemPackages = [
                 inputs.nixvim.packages.${pkgs.stdenv.targetPlatform.system}.default
-              ] ++ kaitoPkgs;
+              ]
+              ++ kaitoPkgs;
               services.keyd = {
                 enable = true;
                 keyboards.default = {
@@ -400,7 +402,7 @@
               # this value at the release version of the first install of this system.
               # Before changing this value read the documentation for this option
               # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-              system.stateVersion = "25.11"; # Did you read the comment?
+              system.stateVersion = "25.05"; # Did you read the comment?
 
               # Other options such as
               # hardware.graphics.enable = true
